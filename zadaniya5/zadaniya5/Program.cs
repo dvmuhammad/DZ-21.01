33e540f3d79c80e77255634f277d980dc64622e0
Muhammad <davlatovmuhammad1264@mail.com>
2021-01-28 14:49:22 +0500

    Add zadaniya5/

---
+++ zadaniya5/zadaniya5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace zadaniya5
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int a, b, c, d;
+			a = Convert.ToInt32(Console.ReadLine());
+			b = Convert.ToInt32(Console.ReadLine());
+			c = Convert.ToInt32(Console.ReadLine());
+			d = Convert.ToInt32(Console.ReadLine());
+			if (a < b && c < d && b < c && b < d)
+			{
+				Console.WriteLine("Числа расположены по возрастанию");
+			}
+			else if (a == b && c == d && b == c && b == d)
+			{
+				Console.WriteLine(a * b * c * d);
+			}
+			else if (a < b && a < c && a < d)
+			{
+				Console.WriteLine($"Min:{a}");
+			}
+			else if (b < a && b < c && b < d)
+			{
+				Console.WriteLine($"Min:{b}");
+			}
+			else {
+				int m = Math.Min(c, d);
+				Console.WriteLine($"Min:{m}");
+			}
+
+			Console.ReadKey();
+		}
+	}
+}

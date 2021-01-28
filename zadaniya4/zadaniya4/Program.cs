caad1780fae9c093d164f1d3f7e44fdf9e5071d1
Muhammad <davlatovmuhammad1264@mail.com>
2021-01-28 14:48:51 +0500

    Add zadaniya4/

---
+++ zadaniya4/zadaniya4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace zadaniya4
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int a;
+			a = Convert.ToInt32(Console.ReadLine());
+			if (a >= 500) Console.WriteLine(a - (a * 3) / 100);
+			else if (a > 1000) Console.WriteLine(a - (a * 5) / 100);
+			else if (a < 500) Console.WriteLine(a);
+			Console.ReadKey();
+			                                  
+		}
+	}
+}

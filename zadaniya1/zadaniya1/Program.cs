d848c852604035d0d78d95d1115823ab477a0f40
Muhammad <davlatovmuhammad1264@mail.com>
2021-01-28 14:46:35 +0500

    Add zadaniya1/

---
+++ zadaniya1/zadaniya1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace zadaniya1
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int a, b;
+			a = Convert.ToInt32(Console.ReadLine());
+			b = Convert.ToInt32(Console.ReadLine());
+			if (a != b)
+			{
+				a = a + b;
+				b = a;
+				Console.WriteLine($"{a} {b}");
+			}
+			else
+			{
+				a = b = 0;
+				Console.WriteLine($"{a} {b}");
+			}
+			Console.ReadKey();
+		}     
+	}
+}

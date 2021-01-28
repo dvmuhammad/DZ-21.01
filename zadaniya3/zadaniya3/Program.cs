d23ae88b19e094998b862a141d8bd5f985ae6e16
Muhammad <davlatovmuhammad1264@mail.com>
2021-01-28 14:48:21 +0500

    Add zadaniya3/

---
+++ zadaniya3/zadaniya3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace zadaniya3
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			byte a = Convert.ToByte(Console.ReadLine());
+			if (a < 15) Console.WriteLine("[0 - 14]");
+			else if (a < 36) Console.WriteLine("[15 - 35]");
+			else if (a < 51) Console.WriteLine("[36 - 50]");
+			else if (a < 101) Console.WriteLine("50 - 100]");
+
+			Console.ReadKey();
+		}
+	}
+}
